3907faf61115202bdceefe486f9aaf5d709122f7
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-07-31 23:11:55 +0700

    commit 2

--- vue-lab02/src/components/EventCard.vue
+++ vue-lab02/src/components/EventCard.vue
@@ -6,7 +6,7 @@ import { type Event } from '@/types';
 </script>
 <template>
     <routerLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
-    <div class="cursor-pointer border border-red-600 p-4 w-64 mb-6 
+    <div class="cursor-pointer border border-gray-600 p-4 w-64 mb-6 
     hover:scale-101 hover:shadow-sp">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
@@ -15,14 +15,6 @@ import { type Event } from '@/types';
 </template>
 
 <style scoped>
-.event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
-
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2); 
